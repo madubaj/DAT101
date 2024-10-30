0de16acd28e6b42e94e3323dddb62e0ff7e3d3ee
madubaj <dubajevamariam03@gmail.com>
2024-10-30 20:22:31 +0100

    Finished task 3.1

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -2,42 +2,70 @@
 import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
-printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+printOut("--- Part 1, 2 and 3 ----------------------------------------------------------------------------------------");
+let wakeUpTime = 9; 
+  printOut("Wake up time = " + wakeUpTime + " o'clock");
+if (wakeUpTime == 7) {
   printOut("I can take the bus to school.");
+} else if (wakeUpTime == 8) {
+  printOut("I can the train to school")
+} else {
+printOut("I'll have to take the car to school.");
 }
 printOut(newLine);
 
-printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("--- Part 4 and 5 --------------------------------------------------------------------------------------------");
+let value = 12;
+  printOut("Value = " + value);
+if (value < 0) {
+  printOut("Value is Negative");
+} else if (value > 0) {
+  printOut("Value is Positive");
+} else {
+  printOut("Value is Zero");
+}
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("--- Part 6 and 7 ----------------------------------------------------------------------------------------------");
+const imageSize = Math.floor(Math.random() * 8) + 1;
+  printOut("Image size = " + imageSize + " MP");
+if (imageSize >= 6) {
+  printOut("The image is too large");
+} else if (imageSize >= 4) {
+  printOut("Thank you!");
+} else {
+  printOut("The image is too small");
+}
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const monthList = ["January", "February", "March", "April", "May",
+"June", "July", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+  printOut("Month = " + monthName);
+if (monthName.includes("r")) {
+  printOut("You need to take Vitamin D");
+} else {
+  printOut("You do not need to take Vitamin D");
+}
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30];
+const monthIndex = monthList.indexOf(monthName);
+const days = daysInMonth[monthIndex];
+printOut("It is " + days + " days in " + monthName);
 printOut(newLine);
 
-/* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const galleryClosed = ["March", "May"];
+const tempPremises = "April";
+if (galleryClosed.includes(monthName)) {
+  printOut("The gallery is unfortunately closed in " + monthName + " for refurbishment.");
+} else if (monthName === tempPremises) {
+  printOut("The gallery is open in temporary premises in the building next door.");
+} else {
+  printOut("The gallery is open in " + monthName + "!"); 
+}
 printOut(newLine);
