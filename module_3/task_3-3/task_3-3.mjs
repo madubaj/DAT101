b9c02ed89cd06df4f510638a3f384a54ad9a2fad
madubaj <you@example.com>
2025-04-12 11:38:47 +0200

    completed task 3.3

--- module_3/task_3-3/task_3-3.mjs
+++ module_3/task_3-3/task_3-3.mjs
@@ -3,24 +3,66 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const date = new Date(); 
+const options = {
+    dateStyle: "full",
+};
+
+function dateToday() {
+    printOut(date.toLocaleString("no-NB", options));
+    return date; 
+}
+const todayDate = dateToday(); 
 printOut(newLine);
+
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+function getNumberOfDays(start, end) {
+    const date1 = new Date(start);  
+    const date2 = new Date(end);
+    const oneDay = 1000 * 60 * 60 * 24; 
+    const differenceInTime = date2.getTime() - date1.getTime(); 
+    const differenceInDays = Math.round(differenceInTime / oneDay); 
+    return differenceInDays; 
+}
+const daysLeft = getNumberOfDays(dateToday(), "5/14/2025"); 
+
+printOut("It is " + daysLeft + " days left to the release of Elden Ring: Nightreign"); 
+
 printOut(newLine);
+
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const radius = 10; 
+
+function circle() {
+    let diameter = 2 * radius; 
+    let circumference = (Math.PI * 2) * radius; 
+    let area = Math.PI * radius * radius; 
+    return {diameter, circumference, area}; 
+}
+let result = circle(radius); 
+
+printOut("Diameter: " + Math.floor(result.diameter)); 
+printOut("Circumference: " + Math.floor(result.circumference));
+printOut("Area: " + result.area.toFixed(2)); 
 printOut(newLine);
 
+
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+function rectangle(height, width) {
+    return {
+        circumference: height + width + height + width, 
+        area: width * height
+    }; 
+}
+let results = rectangle(5, 10); 
+
+printOut("Circumference: " + results.circumference);
+printOut("Area: " + results.area);
+
 printOut(newLine);
+
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 //Fahrenheit = (Kelvin - 237.15) * 9/5 + 32;
@@ -35,7 +77,7 @@ function convertTemperature(aTemp, aType) {
     let Kelvin = 0; 
    switch(aType){
     case ETempType.Celsius:
-        printOut("Convert from Celsius");
+        printOut("Convert 38 Celsius");
         //Convert to Fahrenheit 
         //Fahrenheit = (Kelvin - 237.15) * 9/5 + 32
         Celsius = aTemp; 
@@ -43,7 +85,7 @@ function convertTemperature(aTemp, aType) {
         Kelvin = Celsius + 237.15; 
         break;
     case ETempType.Fahrenheit:
-        printOut("Convert from Fahrenheit");
+        printOut("Convert 57 Fahrenheit");
         //Convert to Celsius
         //Celsius = (Fahrenheit - 32) * 5/9)
         Fahrenheit = aTemp; 
@@ -51,37 +93,49 @@ function convertTemperature(aTemp, aType) {
         Kelvin = Celsius + 237.15;
         break;
     case ETempType.Kelvin: 
-        printOut("Convert from Kelvin");
+        printOut("Convert 62 Kelvin");
         //Convert to Celsius 
-        //Kelvin = ???
+        //Celsius = (Kelvin - 237.15);
         Kelvin = aTemp;
+        Fahrenheit = (Kelvin - 237.15) * 9/5 + 32;
+        Celsius = (Kelvin - 237.15);
         break;
-   }// End switch
-
+   }
    printOut("Celsius = " + Celsius.toFixed(0)); 
    printOut("Fahrenheit = " + Fahrenheit.toFixed(0)); 
    printOut("Kelvin = " + Kelvin.toFixed(0)); 
+}
+convertTemperature(38, ETempType.Celsius); printOut("");
+convertTemperature(57, ETempType.Fahrenheit); printOut("");
+convertTemperature(62, ETempType.Kelvin); 
 
-}// End function
-
-convertTemperature(37.5, ETempType.Celsius);
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 
-function calculateNetPrice(aPrice, aTaxGroup){
+printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+function calculateNetPrice(aPrice, aTaxGroup) {
     let net = NaN; 
     let taxGroup = aTaxGroup.toUpperCase();
     let vat = NaN; 
     
-    printOut("tax.Group = " + taxGroup); 
+    printOut("tax-group = " + taxGroup); 
     
-    switch(taxGroup){
+    switch(taxGroup) {
         case "NORMAL":
-        vat = 25;
+            vat = 25;
+            break; 
+        case "15":
+            vat = 15;
+            break; 
+        case "10": 
+            vat = 10;
+            break; 
+        case "Sugar":
+            vat = 0; 
+            break; 
     }
 
-    if(!Number.isNaN(vat)){
+    if(!Number.isNaN(vat)) {
         net = (100 * aPrice) / (vat + 100);
     }
 
@@ -89,84 +143,139 @@ function calculateNetPrice(aPrice, aTaxGroup){
 }
 
 const netPrice1 = calculateNetPrice(100, "normal");
-if(Number.isNaN(netPrice1)){
+if(Number.isNaN(netPrice1)) {
     printOut("Unknown VAT group!");
 }else{
-    printOut("netPrice1 = " + netPrice1.toFixed(2));
-}
+    printOut("100 is " + netPrice1.toFixed(2) + " without tax");
+} 
+printOut(""); 
 
-const netPrice2 = calculateNetPrice(100, "other");
-if(Number.isNaN(netPrice2)){
+const netPrice2 = calculateNetPrice(70, "15");
+if(Number.isNaN(netPrice2)) {
     printOut("Unknown VAT group!");
 }else{
-    printOut("netPrice2 = " + netPrice2.toFixed(2));
-}
+    printOut("70 is " + netPrice2.toFixed(2) + " without tax");
+} 
+printOut(""); 
 
-const netPrice3 = calculateNetPrice(100, "other");
-if(Number.isNaN(netPrice3)){
+const netPrice3 = calculateNetPrice(180, "10");
+if(Number.isNaN(netPrice3)) {
     printOut("Unknown VAT group!");
 }else{
-    printOut("netPrice3 = " + netPrice3.toFixed(2));
+    printOut("180 is " + netPrice3.toFixed(2) + " without tax");
 }
+printOut(""); 
 
-const netPrice4 = calculateNetPrice(100, "other");
-if(Number.isNaN(netPrice4)){
+const netPrice4 = calculateNetPrice(50, "Sugar");
+if(Number.isNaN(netPrice4)) {
     printOut("Unknown VAT group!");
 }else{
-    printOut("netPrice4 = " + netPrice4.toFixed(2));
+    printOut(netPrice4.toFixed(2));
 }
 
 printOut(newLine);
 
+
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*
+Create a function that takes 3 arguments and returns the following calculation:
+    Speed = Distance / Time
+If speed is missing, calculate speed. 
+If time is missing, calculate time. 
+If distance is missing, calculate the distance. 
+If more than one parameter is missing, return NaN. 
+*/
+
+function calculate(missingType, speed, distance, time) {
+
+    switch (missingType) {
+        case "speed":
+            if (time === 0) return NaN;
+            speed = distance / time;
+            break;
+
+        case "distance":
+            distance = speed * time;
+            break;
+
+        case "time":
+            if (speed === 0) return NaN;
+            time = distance / speed;
+            break;
+    }
+
+    printOut("Speed = " + speed + " km/h");
+    printOut("Distance = " + distance + " km");
+    printOut("Time = " + time + " h");
+}
+calculate("speed", null, 50, 2); printOut("");            
+calculate("distance", 6, null, 30); printOut("");        
+calculate("time", 70, 105, null); printOut("");        
+calculate("speed", null, 50, null);                  
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+function returnString(textString, maxLength, param3, param4) {
+        if (textString.length >= maxLength) {
+            return textString;
+        }
+        let difference = maxLength - textString.length;
+        let padding = param3.repeat(difference); 
+        
+        let newTextString = param4 ? padding + textString : textString + padding; 
+        
+        return newTextString;
+}
+printOut(returnString("Text ", 10, " ' ", true));
+printOut(returnString("String ", 10, " ' ", false)); 
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-
-function testIfMathIsFun(){
+function testIfMathIsFun() {
     let op = 1;
-    let line = 1; 
-    // Left side   
+    let line = 1;
     let ok = false;
-    do{
-    let sumLeft  = 0; 
-    for(let left = 0; left < line + 1; left++){
-        sumLeft += op; 
-        op++; 
-    }
-
-    let sumRight = 0;
-    for(let right = 0; right < line; right++){
+    do {
+      let sumLeft = 0;
+      for (let left = 0; left < line + 1; left++) {
+        sumLeft += op;
+        op++;
+      }
+  
+      let sumRight = 0;
+      for (let right = 0; right < line; right++) {
         sumRight += op;
         op++;
-    }
-
-    if(sumLeft !== sumRight){
-        //funksjonen må stoppes på enellerannen måte 
+      }
+  
+      if (sumLeft !== sumRight) {
         ok = false;
-    }else{
+        printOut("Error in line " + line.toString());
+      }else{
         ok = true;
-    }
-    line++;
-
-    }while(ok){
-    };
-}
-
-printOut("Replace this with you answer!");
-
+      }
+      line++;
+  
+      if(line > 200){
+        printOut("Math is Fun!");
+        break;
+      }
+      
+    } while (ok);
+  }
+  testIfMathIsFun();
+  
 printOut(newLine);
 
-/* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+
+const factorial = (n) => {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  };
+  printOut("Factorial (10) is " + factorial(10));
