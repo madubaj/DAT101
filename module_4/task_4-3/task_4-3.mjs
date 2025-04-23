ceb691b642c9383b03d9cbcb8ea5b123ff763bca
madubaj <you@example.com>
2025-04-23 13:57:21 +0200

    update task 4.3

--- module_4/task_4-3/task_4-3.mjs
+++ module_4/task_4-3/task_4-3.mjs
@@ -46,22 +46,111 @@ const MovieGenre = [
 ];
 
 //--- Part 1 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
+const cmbTask1Calculate = document.getElementById("cmbTask1Calculate");
+cmbTask1Calculate.addEventListener("click", cmbTask1CalculateClick);
+function cmbTask1CalculateClick() {
+ const txtRectHeight = document.getElementById("txtRectHeight");
+  const txtRectWidth = document.getElementById("txtRectWidth");
+  const height = Number(txtRectHeight.value);
+  const width = Number(txtRectWidth.value);
+
+  const perimeter = (height + width + height + width);
+  const area = (height * width);
+  const txtTask1Output = document.getElementById("txtTask1Output");
+  txtTask1Output.innerHTML = "Omkrets: " + perimeter + "&nbsp; Areal: " + area;
+}; 
+
 
 //--- Part 2 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
+const txtTask2Word = document.getElementById("txtTask2Word");
+txtTask2Word.addEventListener("keypress", txtTask2WordKeyPress);
+let task2Words = [];
+const txtTask2Output = document.getElementById("txtTask2Output");
+
+function txtTask2WordKeyPress(aEvent) {
+  const key = aEvent.key;
+  switch(key) {
+    case "Enter":
+      const words = txtTask2Word.value.split(" ");
+      txtTask2Word.value = "";
+      task2Words = task2Words.concat(words);
+      txtTask2Output.innerHTML = "Number of words: " + task2Words.length + "<br>" + task2Words.join(" ");
+      console.log(task2Words);
+      break;
+  }
+}
+
 
 //--- Part 3 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
+const cmbTask3CheckAnswer = document.getElementById("cmbTask3CheckAnswer");
+cmbTask3CheckAnswer.addEventListener("click", cmbTask3CheckAnswerClick);
+const txtTask3Output = document.getElementById("txtTask3Output");
+
+let text = "";
+function cmbTask3CheckAnswerClick() {
+  const chkTask3 = document.getElementsByName("chkTask3");
+  for(let i = 0; i < chkTask3.length; i++) {
+   const checkBox = chkTask3[i];
+   if(checkBox.checked){
+    const value = checkBox.value;
+    if(value === "4"){
+      text += "Du har valgt nummer " + value + "."
+
+    }else{
+      text += "Du har valgt nummer " + value + ".<br />"
+    }
+   }
+
+  }
+  txtTask3Output.innerHTML = text;
+  text = "";
+}
+
 
 //--- Part 4 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
+const carDiv = document.getElementById("divTask4Cars");
+const output = document.getElementById("txtTask4Output"); 
+
+function handleCarSelection(event) {
+  if(event.target.checked) {
+    output.textContent = "Du har valgt: " + event.target.value; 
+  }
+}
+
+for (let i = 0; i < CarTypes.length; i++) {
+  const car = CarTypes[i]; 
+
+  const radio = document.getElementById("input"); 
+  radio.type = "radio"; 
+  radio.name = "car";
+  radio.value = car;
+  radio.id = "car_" + i; 
+
+
+  const label = document.createElement("label");
+  label.htmlFor = radio.id; 
+  label.textContent = car; 
+  
+  radio.addEventListener("change", handleCarSelection); 
+
+  carDiv.appendChild(radio);
+  carDiv.appendChild(label); 
+  carDiv.appendChild(document.createElement("br")); 
+}
+
+
+
+/**
+Use a for-loop to add "radio" buttons to the divTask4Cars element. Get the values from the CarTypes
+array. Print the selected car in txtTask4Output.
+ */
+
 
 //--- Part 5 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
+
 
 //--- Part 6 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
 
-//--- Part 7 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
+
+
+//--- Part 7 ----------------------------------------------------------------------------------------------// 
